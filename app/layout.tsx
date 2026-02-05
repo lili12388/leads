213cd02ff56cd6a53c7c3338f2ad617ef5365438
lili12388 <laithou123@gmail.com>
2026-02-06 00:28:03 +0100

    Add Google Tag Manager GTM-TCDNG7LQ

--- app/layout.tsx
+++ app/layout.tsx
@@ -267,6 +267,19 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TCDNG7LQ');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* Meta Pixel Code */}
         <script
           dangerouslySetInnerHTML={{
@@ -320,6 +333,16 @@ export default function RootLayout({
         />
       </head>
       <body className={`font-sans antialiased bg-background text-foreground`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TCDNG7LQ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         
         <Analytics />
